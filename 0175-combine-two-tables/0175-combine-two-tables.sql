9772df134e7810002142e9c29ec8e2bf098255b4
the_poorvaj_567 <gowdapoorvaj8@gmail.com>
2026-03-31 15:46:32 +0530

    Time: 406 ms (73.86%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,2 +1,4 @@
 /* Write your T-SQL query statement below */
-select p.firstName,p.lastName,a.city,a.state from person p left join Address a on p.personId=a.personId;
+select p.firstName,p.lastName,a.city,a.state 
+from person p left join Address a on
+p.personId=a.personId;
